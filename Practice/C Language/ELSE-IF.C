74d563772a1acd68ef773fee164f5eed918057d6
guptaaniket40 <guptaaniket052005@gmail.com>
2025-07-25 14:01:40 +0530

    Practice

---
+++ Practice/C Language/ELSE-IF.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int marks;
+   clrscr();
+   printf("\nEnter marks:");
+   scanf("%d",&marks);
+
+   if(marks>=90)
+   {
+   printf("\nExcellent");
+   }
+   else if(marks>=70)
+   {
+   printf("\nGood");
+   }
+   else if(marks>=50)
+   {
+   printf("\nAverage");
+   }
+   else
+   {
+   printf("\nFail");
+   }
+   getch();
+
+ }
