3e4d27be9ea6b2aba09c80ac82f29a077e2d92e6
guptaaniket40 <guptaaniket052005@gmail.com>
2025-07-28 15:22:51 +0530

    Practice

---
+++ Practice/C Language/CONTINUE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+       int i;
+       clrscr();
+       for(i=0;i<10;i++)
+       {
+	  if(i==5)
+	 {
+	     continue;
+	   }
+	   else{
+		 printf("%d\n",i);
+	     }
+	   }
+	   getch();
+    }
