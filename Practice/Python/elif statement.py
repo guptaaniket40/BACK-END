9900345ebd8224a2fbdb35f26d1b0170f25fd004
guptaaniket40 <guptaaniket052005@gmail.com>
2025-10-26 11:59:27 +0530

    Practice

---
+++ Practice/Python/elif statement.py
@@ -0,0 +1,14 @@
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+c=int(input("Enter c:"))
+
+if a>b:
+    if a>c:
+        print("A is max number")
+    else:
+        print("c is max num")
+        
+elif b>c:
+        print("B is max num")
+else:
+        print("c is max num")
