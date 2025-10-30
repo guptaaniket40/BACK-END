2e9bba8d457fdb9a8a8dbd380faeead001063efe
guptaaniket40 <guptaaniket052005@gmail.com>
2025-10-30 15:40:43 +0530

    Practice

---
+++ Assignment/Fundamental of python/Practical Assignment/Q5.2.py
@@ -0,0 +1,9 @@
+List1 = ['apple', 'banana', 'mango']
+search = input("Enter a fruit to search: ")
+
+for fruit in List1:
+    if fruit == search:
+        print(search, "is found in the list.")
+        break
+else:
+    print(search, "is not found in the list.")
